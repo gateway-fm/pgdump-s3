078a3e72e042623d3d6cbb4ddf83a13e384d9498
Vitalii Shturmin <vitalik@gateway.fm>
2021-10-21 10:40:33 +0300

    removed useless stuff

--- Dockerfile
+++ Dockerfile
@@ -4,9 +4,8 @@ RUN apk add --no-cache py3-pip && pip3 install awscli && mkdir /backup
 
 ENV AWS_DEFAULT_REGION=eu-north-1
 
-COPY entrypoint.sh /usr/local/bin/entrypoint
 COPY backup.sh /usr/local/bin/backup
 
 VOLUME /backup
 
-CMD /usr/local/bin/entrypoint
+CMD /usr/local/bin/backup
--- backup.sh
+++ backup.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 
-BACKUP_NAME="$(date -u +%Y-%m-%d_%H-%M-%S)_UTC.gz"
+set +e
+
+BACKUP_NAME="$(date -u +%Y-%m-%d_%H-%M)_UTC.gz"
 
 # Run backup
 
@@ -12,3 +14,5 @@ cd /backup/ && tar -cvzf "${BACKUP_NAME}" latest.dump
 aws s3 cp "/backup/${BACKUP_NAME}" "s3://${S3_BUCKET}/${BACKUP_NAME}"
 # Delete temp files
 rm -rf /backup/latest.dump
+
+exec "$@"
